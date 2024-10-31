c0326b414a4c38172d6b8197dbe22a8683d4ef46
Jacob Nilsson <jacob@jacobnilsson.com>
2024-10-31 22:33:39 +0100

    Implement a hack which implies tags inside other tags (such as <strong> inside <p>) will be interpreted as text. It can be disabled if unwanted.

--- tests/test.cpp
+++ tests/test.cpp
@@ -1994,6 +1994,25 @@ inline namespace General {
 #ifdef BYGG_USE_LIBXML2
 inline namespace Parser {
     void test_html_parser() {
+        /*
+        const auto test_struct_output = []() {
+            const std::string html = "<html><head><title>Title</title></head><body><p>Paragraph with a <strong>strong</strong> tag in between.</p></body></html>";
+            const auto structure = bygg::parse_html_string(html);
+
+            for (const auto& it : structure) {
+                std::cout << "Tag: " << it.tag << "\n";
+                std::cout << "Data: " << it.data << "\n";
+                std::cout << "Depth: " << it.depth << "\n";
+                std::cout << "Type: " << static_cast<int>(it.type) << "\n";
+                std::cout << "Properties: \n";
+
+                for (const auto& itp : it.properties) {
+                    std::cout << "\t" << itp.get_key() << ": " << itp.get_value() << "\n";
+                }
+            }
+        };
+        */
+
         const auto test_basic = []() {
             using namespace bygg::HTML::Parser;
 
@@ -2024,9 +2043,23 @@ inline namespace Parser {
             REQUIRE(document.get() == html);
         };
 
+        const auto test_with_tags_in_between = []() {
+            using namespace bygg::HTML::Parser;
+
+            const std::string html = "<html><head><title>Title</title></head><body><p>Paragraph with a <strong>strong</strong> tag in between. And an <em>Italic</em> for good measure.</p></body></html>";
+            const auto document = bygg::HTML::Parser::parse_html_string(html);
+
+            REQUIRE(document.get(bygg::HTML::Formatting::Pretty) == "<html>\n\t<head>\n\t\t<title>Title</title>\n\t</head>\n\t<body>\n\t\t<p>Paragraph with a <strong>strong</strong> tag in between. And an <em>Italic</em> for good measure.</p>\n\t</body>\n</html>");
+            REQUIRE(document.get() == html);
+        };
+
+        /*
+        test_struct_output();
+        */
         test_basic();
         test_slightly_more_advanced();
         test_divs();
+        test_with_tags_in_between();
     }
 }
 #endif
