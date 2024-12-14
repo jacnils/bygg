6683444dcfaf623c1ef42c919c5f39c4e36f8db6
Jacob Nilsson <166003882+jcbnilsson@users.noreply.github.com>
2024-12-14 14:30:14 +0100

    Introduce a bygg-cli command line utility (#24)
    Introduce a bygg-cli command line utility. It may be used to generate documents and pseudo-code, in order to save time.

--- README.md
+++ README.md
@@ -14,10 +14,13 @@ Component-based HTML/CSS builder for C++17.
 - Sensible indentation for pretty-formatting.
 - HTML serialization\*
 - No dependencies, other than the standard library\*
+- Command-line tool for generating HTML or pseudo-code from HTML or Markdown files\*
 - Windows, macOS, Linux and \*BSD support\*\*
 
-*Serialization requires the use of libxml2, which is not included in this repository.
+*Serialization and CLI requires the use of libxml2, which is not included in this repository.
 Tests require the use of Catch2, which is not included in this repository.
+Markdown support requires `pandoc` to be installed on the system. Pandoc is not included in this
+repository.
 
 **Not tested on BSD. bygg is actively tested on Windows, macOS and Linux through GitHub Actions.
 
@@ -25,8 +28,9 @@ Tests require the use of Catch2, which is not included in this repository.
 
 - C++17 compiler
 - CMake
-- libxml2 (*optional*, for serialization)
+- libxml2 (*optional*, for CLI and serialization)
 - Catch2 (*optional*, for tests)
+- Pandoc (*optional*, for Markdown support when using the CLI, not required at compile-time)
 
 ## Installation
 
@@ -46,6 +50,10 @@ If you're packaging the library, `/usr` is probably a more suitable prefix.
 If you don't need serialization, you can disable it along with the libxml2
 dependency by passing `-DBYGG_USE_LIBXML2=OFF` to CMake. This will disable the
 serialization features of the library and not install the headers for it.
+Note that you must disable the CLI as well.
+
+If you don't need the CLI, you can disable it by passing `-DBYGG_BUILD_CLI=OFF` to CMake.
+This will disable the CLI utility.
 
 If you want to enable the tests, you can pass `-DBYGG_BUILD_TESTS=ON` to 
 CMake, and they will automatically be built.
@@ -104,6 +112,29 @@ But if you find that you need to work with strings in your sections or elements,
 to create them.
 
 See the Doxygen documentation in `docs` for classes and functions that are exposed by the library.
+
+## CLI
+
+The CLI utility can be used to generate HTML or pseudo-code from HTML or Markdown files.  This may be useful 
+if you want to quickly generate `bygg` code from an existing HTML or even Markdown document. Note that you 
+can also read from stdin.
+
+If you enabled the CLI during the build process and executed `cmake --install . --prefix /usr`, you should
+be able to simply run `bygg-cli` from the command line. In addition, if you enabled `BYGG_INSTALL_DOCS`,
+you can run `man bygg-cli` (provided you have `man` installed) to see the full documentation for the CLI.
+
+Example usage:
+
+```sh
+bygg-cli --formatting pretty --input html input.html # generates pretty-formatted HTML from input.html
+bygg-cli --formatting pretty --input markdown input.md # generates pretty-formatted HTML from input.md
+bygg-cli --formatting pseudo --input html input.html # generates pseudo-code from input.html
+bygg-cli --formatting pseudo --input markdown input.md # generates pseudo-code from input.md
+cat input.html | bygg-cli --formatting pretty --input html # generates pretty-formatted HTML from stdin
+cat input.md | bygg-cli --formatting pretty --input markdown # generates pretty-formatted HTML from stdin
+```
+
+See `bygg-cli --help` or the aforementioned man page for more information.
 
 ## License
 
---
+++ src/cli/bygg-cli.cpp
@@ -0,0 +1,188 @@
+/*
+ * bygg - Component-based HTML/CSS builder for C++
+ *
+ * Copyright 2024 - Jacob Nilsson & contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string_view>
+#include <unistd.h>
+#include <filesystem>
+#include <bygg/HTML/HTML.hpp>
+#include <bygg/HTML/parser.hpp>
+
+#ifndef BYGG_VERSION
+#define BYGG_VERSION "0.0.0"
+#endif
+
+#ifdef _WIN32
+        const std::string temp_file{"temp_data"};
+#else
+        const std::string temp_file{"/tmp/temp_data"};
+#endif
+
+enum class InputType {
+    HTML,
+    Markdown,
+};
+
+int main(int argc, char** argv) {
+    bygg::HTML::Formatting formatting = bygg::HTML::Formatting::Pretty;
+    InputType input_type = InputType::HTML;
+    bool pseudocode{false};
+    std::string input_file{};
+
+    const std::vector<std::string_view> args{argv, argv + argc};
+
+    for (int i{1}; i < args.size(); ++i) {
+        if (args.at(i) == "-h" || args.at(i) == "--help" || args.at(i) == "/h") {
+            std::cout << "bygg-cli - a command-line interface for bygg\n";
+            std::cout << "usage: bygg-cli [options] [file]\n";
+            std::cout << "options:\n";
+            std::cout << "  -h, --help, /h: show this help message\n";
+            std::cout << "  -v, --version, /v: show the version of bygg-cli\n";
+            std::cout << "  -c, --copyright, /c: show the copyright information\n";
+            std::cout << "  -f, --formatting, /f: set the formatting type (none, pretty, newline, pseudo)\n";
+            std::cout << "  -i, --input, /i: set the input type (html, markdown)\n";
+            std::cout << "  file: the file to read from\n";
+            std::cout << "if no file is specified, input will be read from stdin\n";
+            std::cout << "if no formatting type is specified, the default is pretty\n";
+            std::cout << "if no input type is specified, the default is html\n";
+            return 0;
+        } else if (args.at(i) == "-v" || args.at(i) == "--version" || args.at(i) == "/v") {
+            std::cout << "bygg-cli version " << BYGG_VERSION << "\n";
+            return 0;
+        } else if (args.at(i) == "-c" || args.at(i) == "--copyright" || args.at(i) == "/c") {
+            std::cout << "bygg-cli - a command-line interface for bygg\n";
+            std::cout << "by Jacob Nilsson & contributors\n";
+            std::cout << "licensed under the MIT license\n";
+            return 0;
+        } else if (args.at(i) == "-f" || args.at(i) == "--formatting" || args.at(i) == "/f") {
+            if (argc >= i + 1) {
+                if (args.at(i+1) == "none") {
+                    formatting = bygg::HTML::Formatting::None;
+                } else if (args.at(i+1) == "pretty") {
+                    formatting = bygg::HTML::Formatting::Pretty;
+                } else if (args.at(i+1) == "newline") {
+                    formatting = bygg::HTML::Formatting::Newline;
+                } else if (args.at(i+1) == "pseudo") {
+                    pseudocode = true;
+                } else {
+                    std::cerr << "invalid formatting type: " << args.at(i+1) << "\n";
+                    std::cerr << "valid types: none, pretty, newline, pseudo" << "\n";
+                    return 1;
+                }
+            } else {
+                std::cout << "no formatting type specified." << "\n";
+            }
+
+            ++i;
+        } else if (args.at(i) == "-i" || args.at(i) == "--input" || args.at(i) == "/i") {
+            if (argc >= i + 1) {
+                if (args.at(i+1) == "html") {
+                    input_type = InputType::HTML;
+                } else if (args.at(i+1) == "markdown") {
+                    input_type = InputType::Markdown;
+                } else {
+                    std::cerr << "invalid input type: " << args.at(i+1) << "\n";
+                    std::cerr << "valid types: html, markdown" << "\n";
+                    return 1;
+                }
+            } else {
+                std::cout << "no input type specified." << "\n";
+            }
+
+            ++i;
+        } else {
+            if (input_file.empty()) {
+                input_file = std::string{args.at(i)};
+            } else {
+                std::cerr << "invalid argument: " << args.at(i) << "\n";
+                return 1;
+            }
+        }
+    }
+
+    if (input_file.empty()) {
+        if (isatty(fileno(stdin))) {
+            std::cerr << "no input file specified." << "\n";
+            return 1;
+        }
+
+        std::string data{};
+        std::string line{};
+        while (std::getline(std::cin, line)) {
+            data += line + "\n";
+        }
+
+        std::ofstream file{temp_file};
+        if (!file.is_open()) {
+            std::cerr << "failed to open temporary file: " << temp_file << "\n";
+            return 1;
+        }
+
+        file << data;
+
+        file.close();
+
+        input_file = temp_file;
+    }
+
+    std::string html{};
+    if (input_type == InputType::Markdown) {
+        std::string command{"pandoc -f markdown -t html " + input_file};
+
+        FILE* pipe = popen(command.c_str(), "r");
+        if (!pipe) {
+            std::cerr << "pandoc failed to convert Markdown to HTML. is it installed?\n";
+            if (input_file == temp_file) {
+                std::remove(temp_file.data());
+            }
+            return 1;
+        }
+
+        char buffer[128];
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != nullptr) {
+                html += buffer;
+            }
+        }
+
+        pclose(pipe);
+    } else {
+        std::ifstream file{input_file};
+        if (!file.is_open()) {
+            std::cerr << "failed to open file: " << input_file << "\n";
+            if (input_file == temp_file) {
+                std::remove(temp_file.data());
+            }
+            return 1;
+        }
+
+        html = {std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+
+        file.close();
+    }
+
+    if (html.empty()) {
+        std::cerr << "buffer is empty: " << input_file << "\n";
+
+        if (input_file == temp_file) {
+            std::remove(temp_file.data());
+        }
+
+        return 1;
+    }
+
+    if (std::filesystem::exists(temp_file)) {
+        std::remove(temp_file.data());
+    }
+
+    bygg::HTML::Section section = bygg::HTML::Parser::parse_html_string(html);
+
+    std::cout << (pseudocode ? bygg::HTML::generate_pseudocode(section) : section.get(formatting)) << "\n";
+
+    return 0;
+}
